7b9d5afd6a8c8097b1d1af8995d513df2bd90339
Lyndon Michael Bibera <secret.transaction@gmail.com>
2014-07-15 10:18:48 +0800

    tried some simple begginer stuffs learned from: http://www.afterhoursprogramming.com/tutorial/Python/If-Statement/
    Signed-off-by: Lyndon Michael Bibera <secret.transaction@gmail.com>

--- appengine-try-python-django/hello/views.py
+++ appengine-try-python-django/hello/views.py
@@ -1,4 +1,29 @@
 from django import http
 
+'''
+
+huge ass comments
+
+hahaha
+
+
+'''
 def home(request):
-    return http.HttpResponse('Python World!')
+    #im a comment
+    print("Test Huge Ass Print")
+
+    a = 300
+    b = 200
+    c = a * b
+
+    s = "Who is the greatest???"
+    if a > b:
+        s = "A is Great"
+    elif b > a:
+        s = "B is Great"
+    else:
+        s = "I dont know who's greater..."
+
+    print(s)
+
+    return http.HttpResponse(s)
