f47d0a927ebe8aca4725c057de9c7b65e202de9c
Lyndon Michael Bibera <secret.transaction@gmail.com>
2014-07-15 17:21:45 +0800

    added some stuffs about lists and for loops
    Signed-off-by: Lyndon Michael Bibera <secret.transaction@gmail.com>

--- appengine-try-python-django/hello/views.py
+++ appengine-try-python-django/hello/views.py
@@ -1,13 +1,15 @@
 from django import http
 
-'''
+"""
 
 huge ass comments
 
 hahaha
 
 
-'''
+"""
+
+
 def home(request):
     #im a comment
     print("Test Huge Ass Print")
@@ -16,7 +18,6 @@ def home(request):
     b = 200
     c = a * b
 
-    s = "Who is the greatest???"
     if a > b:
         s = "A is Great"
     elif b > a:
@@ -26,4 +27,24 @@ def home(request):
 
     print(s)
 
+    for a in range(1, 10):
+        scumbag(" aaa" + str(a), "bbb " + s + " " + str(c))
+
+    list_sample()
     return http.HttpResponse(s)
+
+
+def scumbag(p1, p2):
+    print ("invoke the scumbag method" + p1 + " --- " + p2)
+
+
+def list_sample():
+    my_list = [1, 2, 3]
+    my_tuple = (1, 2, 3)
+
+    casted_to_list = list(my_tuple)
+
+    print(my_tuple)
+    print(my_list)
+    print(casted_to_list)
+    return my_list
