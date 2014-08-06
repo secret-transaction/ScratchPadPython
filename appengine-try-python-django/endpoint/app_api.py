08bc32e225ef7c9b8ee30e126a455e73f00f2343
Lyndon Michael Bibera <secret.transaction@gmail.com>
2014-08-06 11:51:35 +0800

    added native query and persist new entity
    Signed-off-by: Lyndon Michael Bibera <secret.transaction@gmail.com>

--- appengine-try-python-django/endpoint/app_api.py
+++ appengine-try-python-django/endpoint/app_api.py
@@ -1,45 +1,65 @@
 __author__ = 'Lyndon'
 
-
 import endpoints
+from hello.models import Greeting
 from protorpc import messages
 from protorpc import message_types
 from protorpc import remote
+from google.appengine.ext import db
 
 
 package = 'Hello'
 
 
-class Greeting(messages.Message):
+class GreetingRequest(messages.Message):
     """Greeting that stores a message."""
     message = messages.StringField(1)
 
 
 class GreetingCollection(messages.Message):
     """Collection of Greetings."""
-    items = messages.MessageField(Greeting, 1, repeated=True)
+    items = messages.MessageField(GreetingRequest, 1, repeated=True)
 
+
+class GreetingResponse(messages.Message):
+    greetingId = messages.StringField(1)
 
 STORED_GREETINGS = GreetingCollection(items=[
-    Greeting(message='hello world!'),
-    Greeting(message='goodbye world!'),
+    GreetingRequest(message='hello world!'),
+    GreetingRequest(message='goodbye world!'),
 ])
+
 
 @endpoints.api(name='helloworld', version='v1')
 class HelloWorldApi(remote.Service):
     """Helloworld API v1."""
 
+    ID_RESOURCE = endpoints.ResourceContainer(
+        message_types.VoidMessage, id=messages.IntegerField(1, variant=messages.Variant.INT32))
+
     @endpoints.method(message_types.VoidMessage, GreetingCollection,
                       path='hellogreeting', http_method='GET',
                       name='greetings.listGreeting')
     def greetings_list(self, unused_request):
-        return STORED_GREETINGS
+        all_greetings = db.GqlQuery('SELECT * FROM Greeting')
 
-    ID_RESOURCE = endpoints.ResourceContainer(
-            message_types.VoidMessage,
-            id=messages.IntegerField(1, variant=messages.Variant.INT32))
+        my_list = []
+        for g in all_greetings:
+            my_list.append(GreetingRequest(message=g.content))
 
-    @endpoints.method(ID_RESOURCE, Greeting,
+        return GreetingCollection(items=my_list)
+
+    @endpoints.method(GreetingRequest, GreetingResponse,
+                      path='greet', http_method='POST',
+                      name='greetings.createGreeting')
+    def create_greeting(self, request):
+        g = Greeting(content=request.message)
+        g.put()
+
+        return GreetingResponse(greetingId=str(g.key()))
+
+
+    @endpoints.method(ID_RESOURCE, GreetingRequest,
                       path='hellogreeting/{id}', http_method='GET',
                       name='greetings.getGreeting')
     def greeting_get(self, request):
