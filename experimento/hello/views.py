00d2f8052c04017253a1b5ea7d807eb6317e00bb
Lyndon Michael Bibera <secret.transaction@gmail.com>
2014-08-18 13:30:34 +0800

    experimented with adding 3rd-party libs
    Signed-off-by: Lyndon Michael Bibera <secret.transaction@gmail.com>

--- experimento/hello/views.py
+++ experimento/hello/views.py
@@ -1,10 +1,15 @@
+import sys
+
+sys.path.insert(0, 'libs')
+
 from datetime import datetime, timedelta
-from Crypto.Hash import SHA256
+#from Crypto.Hash import SHA256
 #from Crypto.PublicKey import RSA
 #from Crypto.Signature import PKCS1_v1_5
 # TODO: work on actual implementation later, priority is client completion
 #from OpenSSL import crypto
 from django import http
+from bs4 import BeautifulSoup
 
 def home(request):
     return http.HttpResponse('Hello World!')
